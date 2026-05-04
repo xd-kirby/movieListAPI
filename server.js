67170dbafe233ba1606916b268b7d88b2046bb93
xd-kirby <alexhuica01@gmail.com>
2026-05-04 12:25:53 +0200

    adding first route for testing

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
-//movielistAPI
+const express = require("express");
+//const sqlite3 = require("sqlite3");
+const app = express();
+const PORT = 3000;
+app.use(express.json());
 
-//testing that everything works now
+app.get("/", (req, res) => {
+  res.send("Hello, from the express app that will soon be handed in!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
